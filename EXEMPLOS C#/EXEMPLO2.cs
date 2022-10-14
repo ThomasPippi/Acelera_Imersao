e5fe436822975161e5fe9fd71563c6f559eb7e75
Thomas Pippi <112496483+ThomasPippi@users.noreply.github.com>
2022-10-14 09:38:36 -0300

    Add files via upload

---
+++ EXEMPLOS C#/EXEMPLO2.cs
@@ -0,0 +1,10 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      int i;
+    Console.WriteLine("Mostrando o valor de 0 a 10. \n");
+    for (i=0; i <= 10; i++){
+        Console.WriteLine(i);
+    }
+  }
+}
---
+++ EXEMPLOS C#/EXEMPLO5.cs
@@ -0,0 +1,20 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      int i, valor1, valor2;
+    Console.WriteLine("Mostrando o intervalo entre dois valores inteiros. \n");
+    Console.WriteLine("Digite o Valor 1:");
+    valor1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o Valor 2:");
+    valor2 = int.Parse(Console.ReadLine());
+    if(valor1 < valor2){
+        for (i = valor1+1; i < valor2; i++){
+            Console.WriteLine(i);
+        }
+    }else{
+        for (i = valor1-1; i > valor2; i--){
+            Console.WriteLine(i);
+        }
+    }
+  }
+}
