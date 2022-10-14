aef2d32918eed83846f6879aab17aea585b2c674
Thomas Pippi <112496483+ThomasPippi@users.noreply.github.com>
2022-10-14 11:48:11 -0300

    Add files via upload
    Exercícios SEMANA 1 - Lógica de Programação

---
+++ EXERCICIOS_SEMANA1/EXERCICIO_1.cs
@@ -0,0 +1,29 @@
+using System;
+class Exercicio_1 {
+  static void Main() {
+      int n1 = 0, n2 = 0, n3 = 0, n4 = 0, media = 7;
+      
+        Console.WriteLine("Caldular média e imprimir superiores a média 7. \n");
+        Console.WriteLine("Digite o valor da nota 1: ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da nota 2: ");
+        n2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da nota 3: ");
+        n3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da nota 4: ");
+        n4 = int.Parse(Console.ReadLine());
+        media = (n1 + n2 + n3 + n4) / 4;
+        if(n1 > media){
+            Console.WriteLine(n1 + " é maior que a MÉDIA. \n");
+        }
+        if(n2 > media){
+            Console.WriteLine(n2 + " é maior que a MÉDIA. \n");
+        }
+        if(n3 > media){
+            Console.WriteLine(n3 + " é maior que a MÉDIA. \n");
+        }
+        if(n4 > media){
+            Console.WriteLine(n4 + " é maior que a MÉDIA. \n");
+        }
+  }
+}
---
+++ EXERCICIOS_SEMANA1/EXERCICIO_2.cs
@@ -0,0 +1,28 @@
+using System;
+class Exercicio_1 {
+  static void Main() {
+      int horas_aula_prof1 = 0, horas_aula_prof2 = 0;
+      double valor_hora_prof1 = 0, valor_hora_prof2 = 0, salario1 = 0, salario2 = 0;
+        Console.WriteLine("Qual professor recebe mais. \n");
+        Console.WriteLine("Horas aulas dada pelo professor 1:");
+        horas_aula_prof1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Valor da aula do professor 1:");
+        valor_hora_prof1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Horas aulas dada pelo professor 2:");
+        horas_aula_prof2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Valor da aula do professor 2:");
+        valor_hora_prof2 = double.Parse(Console.ReadLine());
+        salario1 = horas_aula_prof1 * valor_hora_prof1;
+        salario2 = horas_aula_prof2 * valor_hora_prof2;
+        Console.WriteLine("Salário 1:" + salario1);
+        Console.WriteLine("Salário 1:" + salario2);
+        if(salario1 > salario2){
+            Console.WriteLine("O professor 1 recebe mais.");
+        }else if(salario1 < salario2){
+            Console.WriteLine("O professor 2 recebe mais.");
+        }else{
+            Console.WriteLine("Os professores recebem o mesmo valor.");
+        }
+        
+  }
+}
