5b6af4b690413cdc4ec16d8fa4ceb384c9b3d59e
Thomas Pippi <112496483+ThomasPippi@users.noreply.github.com>
2022-10-17 11:56:11 -0300

    Add files via upload
    Exibindo todos do início ao fim, inserido pelo usuário.

---
+++ EXERCICIOS_SEMANA1/EXERCICIO_14.cs
@@ -0,0 +1,14 @@
+using System;
+class EXERCICIO_14 {
+  static void Main() {
+      int i, inicio = 0, fim = 0;
+    Console.WriteLine("Mostrando todos os valores dentro do intervalo definido. \n");
+    Console.WriteLine("Digite o valor inicial: ");
+    inicio = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o valor final: ");
+    fim = int.Parse(Console.ReadLine());
+    for(i = inicio; i <= fim; i++){
+        Console.WriteLine(i);
+    }
+  }
+}
