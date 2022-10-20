6b3ae491a6ba72ae62ff7b4f0ba0fc926d73bacc
Thomas Pippi <112496483+ThomasPippi@users.noreply.github.com>
2022-10-20 09:55:32 -0300

    Add files via upload

---
+++ EXEMPLOS SEMANA 1/EXEMPLO_03.cs
@@ -0,0 +1,11 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      int i;
+    Console.WriteLine("Mostrando o valor de 20 a 5. \n");
+    for (i=20; i >= 5; i--){
+        Console.WriteLine(i);
+    }
+    
+  }
+}
