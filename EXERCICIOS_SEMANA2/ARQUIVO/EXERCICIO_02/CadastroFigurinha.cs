e38c32f8c58b6f4dbc618f197ed85adfbc13589f
Thomas Pippi <112496483+ThomasPippi@users.noreply.github.com>
2022-10-26 12:13:52 -0300

    Add files via upload

---
+++ EXERCICIOS_SEMANA2/ARQUIVO/EXERCICIO_02/CadastroFigurinha.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figurinha_Copa
+{
+    internal class CadastroFigurinha
+    {
+        StreamWriter sw;
+        StreamReader sr;
+
+        public void fechaArquivo()
+        {
+            sw.Close();
+        }
+        public void criaAbreArquivoFaltante()
+        {
+            sw = new StreamWriter("C:\\Arquivo\\faltante.csv", true, Encoding.UTF8);
+        }
+        public void criaAbreArquivoRepetida()
+        {
+            sw = new StreamWriter("C:\\Arquivo\\repetidas.csv", true, Encoding.UTF8);
+        }
+        public void cadastraFaltante(string codigoFiruginha, string selecaoFigurinha, string jogadorFigurinha)
+        {
+            sw.WriteLine(codigoFiruginha + ";" + selecaoFigurinha + ";" + jogadorFigurinha);
+        }
+        public void cadastraRepetida(string codigoFiruginha, string selecaoFigurinha, string jogadorFigurinha)
+        {
+            sw.WriteLine(codigoFiruginha + ";" + selecaoFigurinha + ";" + jogadorFigurinha);
+        }
+        public void listarRepetida()
+        {
+            string linha;
+            sr = new StreamReader("C:\\Arquivo\\repetidas.csv");
+            linha = sr.ReadLine();
+            while(linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        public void listarFaltante()
+        {
+            string linha;
+            sr = new StreamReader("C:\\Arquivo\\faltante.csv");
+            linha = sr.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = sr.ReadLine();
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ EXERCICIOS_SEMANA2/ARQUIVO/EXERCICIO_02/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figurinha_Copa
+{
+    internal class Figurinha
+    {
+        static void Main(string[] args)
+        {
+            int op = 0, laco = 0;
+            string codigoFigurinha, selecaoFigurinha, jogadorFigurinha;             
+            CadastroFigurinha cadastro = new CadastroFigurinha();
+            Console.WriteLine("\nMenu de Interação.");
+            while(laco == 0)
+            {
+                Console.WriteLine("1 - Cadastrar figurinha FALTANTE.\n" +
+                    "2 - Cadastrar figurinha REPETIDA.\n" +
+                    "3 - Listar figurinhas REPETIDAS.\n" +
+                    "4 - Listar figurinhas FALTANTES.\n" +
+                    "5 - SAIR");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        cadastro.criaAbreArquivoFaltante();
+                        Console.WriteLine("Digite o código da figurinha: ");
+                        codigoFigurinha = Console.ReadLine();
+                        Console.WriteLine("Digite a Seleção do jogador: ");
+                        selecaoFigurinha = Console.ReadLine();
+                        Console.WriteLine("Digite o jogador da figurinha: ");
+                        jogadorFigurinha = Console.ReadLine();
+                        cadastro.cadastraFaltante(codigoFigurinha, selecaoFigurinha, jogadorFigurinha);
+                        cadastro.fechaArquivo();
+                        break;
+                    case 2:
+                        cadastro.criaAbreArquivoRepetida();
+                        Console.WriteLine("Digite o código da figurinha: ");
+                        codigoFigurinha = Console.ReadLine();
+                        Console.WriteLine("Digite a Seleção do jogador: ");
+                        selecaoFigurinha = Console.ReadLine();
+                        Console.WriteLine("Digite o jogador da figurinha: ");
+                        jogadorFigurinha = Console.ReadLine();
+                        cadastro.cadastraRepetida(codigoFigurinha, selecaoFigurinha, jogadorFigurinha);
+                        cadastro.fechaArquivo();
+                        break;
+                    case 3:
+                        cadastro.listarRepetida();
+                        break;
+                    case 4:
+                        cadastro.listarFaltante();
+                        break;
+                    default:
+                        laco = 1;
+                        break;
+                }        
+            }
+            Console.WriteLine("Fim da Operação!");
+        }
+    }
+}
