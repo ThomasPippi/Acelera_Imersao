aab80e885c18d8894dd8eb5e095adc7ed1945b20
Thomas Pippi <thomaspippi.dev@gmail.com>
2022-11-09 11:09:13 -0300

    Atualizado

--- EXERCICIOS_JS/EXERCICIOS_LISTA/EXERCICIO_10/config.js
+++ EXERCICIOS_JS/EXERCICIOS_LISTA/EXERCICIO_10/config.js
@@ -2,12 +2,10 @@ function pessoaCPF(){
     var pessoa = document.getElementById('trocapessoa');
     document.getElementById("pessoa").disabled = false;
     pessoa.innerText = 'CPF:';
-    document.getElementById('data_nasc').style.display = 'inline';
-    document.getElementById('data').style.display = 'inline';
+    document.getElementById('nascimento').style.display = 'inline';
 }
 function pessoaCNPJ(){
     var pessoa = document.getElementById('trocapessoa');
     pessoa.innerText = 'CNPJ:';
-    document.getElementById('data_nasc').style.display = 'none';
-    document.getElementById('data').style.display = 'none';
+    document.getElementById('nascimento').style.display = 'none';    
 }
